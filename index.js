c3e0ba09901701e329a3812143af7d2498107f57
Ink008 <shadowink008@gmail.com>
2024-08-12 11:10:58 +0700

    Add Web Socket

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const http = require('http');
+const { v4: uuidv4 } = require('uuid');
+const url = require("url")
+const { WebSocketServer } = require('ws');
 
 const admin = require('./controller/admin');
 const user = require('./controller/user');
@@ -11,6 +14,7 @@ const task = require('./controller/task');
 
 const app = express();
 const server = http.createServer(app);
+const wss = new WebSocketServer({ server: server });
 const port = 8080;
 
 app.use(function (req, res, next) {
@@ -27,6 +31,54 @@ app.use('/workspace', workspace);
 app.use('/view', view);
 app.use('/status', status);
 app.use('/task', task);
+
+//Web Socket Part
+const connections = {};
+const views = {};
+
+wss.on("connection", (connection, request) => {
+    const uuid = uuidv4();
+    const host = request.headers.host;
+    connections[uuid] = connection;
+    
+    const [, controller, id] = url.parse(request.url, true).pathname.split('/');
+    if(controller == 'view') {
+        views[uuid] = {
+            id: id
+        };
+    }
+
+    console.log(`Instance ${uuid} connected`);
+
+    connection.on('message', async (message) => {
+        let req = JSON.parse(message);
+        try {
+            if(req.message == 'Content Change') {
+                let res = await fetch(`http://${host}/view/${id}/content`);
+                if(res.ok) {
+                    let data = await res.json();
+                    // BroadCast cho client có cùng view trừ client vừa gửi
+                    Object.keys(views).forEach((view_uuid) => {
+                        if(views[view_uuid].id == id && view_uuid != uuid) {
+                            connections[view_uuid].send(JSON.stringify({
+                                message: req.message,
+                                content: data
+                            }));
+                        }
+                    })
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
+    connection.on('close', () => {
+        delete connections[uuid];
+        delete views[uuid];
+        console.log(`Instance ${uuid} disconnected`)
+    });
+});
 
 server.listen(port, () => {
     console.log(`Starting on http://localhost:${port}`);
